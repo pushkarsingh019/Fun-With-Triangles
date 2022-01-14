b04df8f5f91df507a7f154185e197737cbc473f6
Pushkar Singh <stoazest@gmail.com>
2022-01-14 19:22:33 +0530

    Quiz Part done

---
+++ JavaScript Files/quiz.js
@@ -0,0 +1,21 @@
+const quizForm = document.querySelector('.quiz-form');
+const submitBtn = document.querySelector('#submit-btn');
+const scoreDiv = document.querySelector('.output');
+
+const correctAnswers = ['90°', 'right angled'];
+
+function calculateScore(e) {
+	e.preventDefault();
+	const data = new FormData(quizForm);
+	let index = 0,
+		score = 0;
+	for (let entry of data.values()) {
+		if (entry === correctAnswers[index]) {
+			score++;
+		}
+		index++;
+	}
+	scoreDiv.innerText = `Your score is ${score}`;
+}
+
+submitBtn.addEventListener('click', calculateScore);
